264570fac9a296996c1c9adf42944fdb1d936124
michielpost <michiel@q42.nl>
2019-11-01 10:49:04 +0100

    read level

--- js/level.js
+++ js/level.js
@@ -6,7 +6,26 @@ export default class Level {
 
 	activate(){
 		console.log('Activate level', this.micrio.id);
+
+		//TODO: Load level based on micrioId
+		fetch("/levels/level1.json")
+			.then(response => response.json())
+			.then(json => this.startLevel(json));
+
 	}
+
+	startLevel(level) {
+		console.log("Start level:" + level.name);
+		this.level = level;
+
+	}
+
+	talkTo(itemId)
+	{
+		var item = level.items
+	}
+
+	
 
 	deactivate(){
 		console.log('Deactivate level', this.micrio.id);
