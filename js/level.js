8b2c17af10807bb6efb417c2768d7c587bfdeca6
michielpost <michiel@q42.nl>
2019-11-01 14:46:12 +0100

    some conversation

--- js/level.js
+++ js/level.js
@@ -20,9 +20,6 @@ export default class Level {
 	startLevel(level) {
 		console.log("Start level:" + level.name);
 		this.level = level;
-
-		this.talkTo("id-from-micrio-editor");
-		this.say("id-from-micrio-editor", 1);
 	}
 
 	clickedItem(marker) {
@@ -33,8 +30,12 @@ export default class Level {
 		if(item != null)
 		{
 			//Render possible actions in item.actions
-			var actions = item.actions.map(function (action) {
-				return action.name
+			var actions = item.actions
+			.filter(function (action) {
+				return action.isDefault == true;
+			})
+			.map(function (action) {
+				return action.input
 			});
 
 			if(!marker._actions)
@@ -44,8 +45,34 @@ export default class Level {
 		}
 	}
 
+	//Reply to item with a selected reply
 	actionItem(marker, action) {
 		console.log('action item!', action, marker);
+
+		var itemId = marker.id;
+		var item = this.getItemForId(itemId);
+		if(item != null)
+		{
+			var actions = item.actions.filter(function (item) {
+				return item.input == action;
+			});
+
+			var action = actions[0];
+			if(action != null)
+			{
+				console.log("Selected action", action);
+
+				//Render reply
+				//TODO: get X Y for micrio item: getMicrioObjectForId(itemId) implementeren
+				this.printText(action.output, 0, 0);
+
+				if(action.continue)
+					this.renderConversationOptions(marker, item, action.continue);
+
+				//TODO: run custom script if available
+			}
+		}
+
 	}
 
 	getItemForId(itemId) {
@@ -60,26 +87,14 @@ export default class Level {
 		//TODO: How to get Micrio object for id?
 	}
 
-	//Initiate a conversation with item
-	talkTo(marker)
-	{
-		var itemId = marker.id;
-		var item = this.getItemForId(itemId);
-		if(item != null)
-		{
-			//Render options
-			this.renderConversationOptions(marker, item, item.defaultConversationOptions);
-		}
-	}
-
 	//Renders a box to select a conversation item
 	renderConversationOptions(marker, item, idList) {
 		//TODO: Render dialog with all conversation options
 		if(item != null)
 		{
-			var replies = item.conversations.filter(function (item) {
+			var replies = item.actions.filter(function (action) {
 				//TODO: Also filter on inventory items
-				return idList.includes(item.id);
+				return idList.includes(action.id);
 			})
 			.map(function (reply) {
 				return reply.input
@@ -96,34 +111,6 @@ export default class Level {
 	printText(string, x, y) {
 		new Text(this.micrio, string, x, y);
 	}
-
-	//Reply to item with a selected reply
-	say(itemId, selectedId)
-	{
-		var item = this.getItemForId(itemId);
-		if(item != null)
-		{
-			var replies = item.conversations.filter(function (item) {
-				return item.id == selectedId;
-			});
-
-			var reply = replies[0];
-			if(reply != null)
-			{
-				console.log("Using reply", reply);
-
-				//Render reply
-				//TODO: get X Y for micrio item: getMicrioObjectForId(itemId) implementeren
-				this.printText(reply.output, 0, 0);
-
-				if(reply.continue)
-					this.renderConversationOptions(item, reply.continue);
-
-				//TODO: run custom script if available
-			}
-		}
-	}
-
 
 	deactivate(){
 		console.log('Deactivate level', this.micrio.id);
