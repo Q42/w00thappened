daceb6f497d022cba2323f4451d2f2f2c4fa1762
Marcel Duin <marcel@q42.nl>
2019-11-04 12:05:34 +0100

    internal dialog is back

--- js/level.js
+++ js/level.js
@@ -36,8 +36,8 @@ export default class Level {
 		this.level = level;
 
 		// only show intro text first time you enter the scene
-		if (level.dialog && this.game.hasPlayedIntroTexts.indexOf(this.micrio.id) === -1) {
-			this.showDialog(level.dialog);
+		if (level['dialog'] && this.game.hasPlayedIntroTexts.indexOf(this.micrio.id) === -1) {
+			this.showDialog(level['dialog']);
 			this.game.hasPlayedIntroTexts.push(this.micrio.id);
 		}
 	}
