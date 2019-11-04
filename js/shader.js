d4e2b20d339cc2b06dc2a8f9150222947522c55c
Marcel Duin <marcel@q42.nl>
2019-11-04 12:13:51 +0100

    removed compiled shader comments

--- js/shader.js
+++ js/shader.js
@@ -13,10 +13,10 @@ export const fragmentShader = `
 	}
 
 	void main(void)
-	{
+	{`
 		/* Read a group of texels, doing a box blur */
 
-		vec3 accumulator = vec3(0.0);
+		+`vec3 accumulator = vec3(0.0);
 		vec2 dim_step = vec2(1.0/480.0) * 1.4;
 
 		for (int i = -2; i < 2; ++i) {
@@ -25,57 +25,43 @@ export const fragmentShader = `
 			}
 		}
 
-		vec3 blur = accumulator / 16.0;
+		vec3 blur = accumulator / 16.0;`
 
 		/* Convert (RGB source) to greyscale luminance, by taking the Y
 		 * component of YUV (multiplying by BT.709 coefficients) */
 
-		vec3 coefficients = vec3(0.2126, 0.7152, 0.0722);
-		float grey = dot(blur, coefficients);
+		+`vec3 coefficients = vec3(0.2126, 0.7152, 0.0722);
+		float grey = dot(blur, coefficients);`
 
 		/* Adjust brightness */
 
-		float brightness = 1.1;
-		grey = clamp(grey * brightness, 0.0, 1.0);
+		+`float brightness = 1.1;
+		grey = clamp(grey * brightness, 0.0, 1.0);`
 
 		/* Posterize */
 
-		float levels = (6.0) - 1.0;
-		float posterized = floor((grey * levels) + 0.5) / levels;
+		+`float levels = (6.0) - 1.0;
+		float posterized = floor((grey * levels) + 0.5) / levels;`
 
 		/* Increase contrast */
 
-		float contrast = 1.4;
-		float contrasted = clamp(contrast * (posterized - 0.5) + 0.5, 0.0, 1.0);
+		+`float contrast = 1.4;
+		float contrasted = clamp(contrast * (posterized - 0.5) + 0.5, 0.0, 1.0);`
 
 		/* Colourize! Choose a particular hue/saturation, and then pick colours
 		 * by setting the lightness to the calculated value above */
 
-		vec4 rgba = texture2D(map, vUv);
+		+`vec4 rgba = texture2D(map, vUv);
 
 		vec3 rgb = hsl2rgb(vec3(0.2, 0.4, contrasted));
-		gl_FragColor = vec4(vec3(rgb), 1.0) * .09 + rgba * .93;
+		gl_FragColor = vec4(vec3(rgb), 1.0) * .09 + rgba * .93;`+
 
 		//float darkness = .5 - (rgb.r+rgb.g+rgb.b)/3.;
 
 		//gl_FragColor.rgb *= 1.-darkness;
 
-		float grey2 = 0.21 * rgba.r + 0.71 * rgba.g + 0.07 * rgba.b;
-
+		`float grey2 = 0.21 * rgba.r + 0.71 * rgba.g + 0.07 * rgba.b;
 		gl_FragColor.rgb *= .25 + .75 * grey2;
-
-
 		gl_FragColor.a = opacity;
 	}
-
-	/*void mainx() {
-		vec4 rgba = texture2D(map, vUv);
-
-		vec3 coefficients = vec3(0.2126, 0.7152, 0.0722);
-		float grey = dot(coefficients, rgba.rgb);
-
-		gl_FragColor = rgba;
-		gl_FragColor.a = opacity;
-		gl_FragColor.r += .5;
-	}*/
 `;
