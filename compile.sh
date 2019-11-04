6613cf6ba510d82cb3c94cf7591d9c50e6804017
Marcel Duin <marcel@q42.nl>
2019-11-04 12:41:26 +0100

    added LICENSE to minified JS

--- compile.sh
+++ compile.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-java -jar closure-compiler-v20190729.jar --language_in ECMASCRIPT_2017 --compilation_level ADVANCED_OPTIMIZATIONS --language_out ES5 --module_resolution BROWSER --js ./js/*.js --process_common_js_modules --js_output_file w00thappened.min.js
+
+#!/bin/sh
+
+targetjs="./w00thappened.min.js"
+
+cat ./LICENSE > $targetjs
+
+echo "(function(){" >> $targetjs
+java -jar closure-compiler-v20190729.jar --language_in ECMASCRIPT_2017 --language_out ES5 --compilation_level ADVANCED_OPTIMIZATIONS --module_resolution BROWSER --js ./js/*.js --process_common_js_modules --js_output_file $targetjs.tmp
+cat $targetjs.tmp >> $targetjs
+echo "})()" >> $targetjs
+
+rm $targetjs.tmp
